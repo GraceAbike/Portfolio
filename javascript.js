97e3bb4464c8854d655a9432e14345539e283ba5
strangeal <bajerijustice@gmail.com>
2022-09-09 11:45:36 -0400

    Add cards section

--- javascript.js
+++ javascript.js
@@ -89,3 +89,41 @@ const myArray = [
     imageleft: 'image-left',
   },
 ];
+
+const section = document.querySelector('#work-section');
+for (let cardIndex = 0; cardIndex < myArray.length; cardIndex += 1) {
+  section.innerHTML = `
+  <div class="background-img1 background-img">
+<div class="card-img">
+<img src="${myArray[cardIndex].image.image1}" alt="placeholder">
+</div>
+<div class="card-text">
+<h2>${myArray[cardIndex].subtitle}</h2>
+<p>
+${myArray[cardIndex].description}
+</p>
+<ul class="list-item">
+  <li class="item">${myArray[cardIndex].langlist.css}</li>
+  <li class="item">${myArray[cardIndex].langlist.html}</li>
+  <li class="item">${myArray[cardIndex].langlist.bootstrap}</li>
+  <li class="Ruby">${myArray[cardIndex].langlist.ruby}</li>
+</ul>
+<button type="button" class="btn">${myArray[cardIndex].btn}</button>
+</div>
+</div>
+ <div class="background-img2 background-img">
+<div class="card-img1">
+<img src="${myArray[cardIndex].image.image1}" alt="placeholder">
+</div>
+<div class="card-text">
+<h2>${myArray[cardIndex].subtitle}</h2>
+<p>${myArray[cardIndex].description}</p>
+<ul class="list-item">
+<li class="item">${myArray[cardIndex].langlist.css}</li>
+<li class="item">${myArray[cardIndex].langlist.html}</li>
+<li class="item">${myArray[cardIndex].langlist.bootstrap}</li>
+<li class="Ruby">${myArray[cardIndex].langlist.ruby}</li>
+</ul>
+<button type="button" class="btn">${myArray[cardIndex].btn}t</button>
+</div>
+</div>

09b4401ea99e59293310be43c906c3b8924de66d
Grace Abike <mara234216@gmail.com>
2022-09-12 14:28:06 +0200

    Add The second background img

--- JS/javascript.js
+++ JS/javascript.js
@@ -26,27 +26,27 @@ const myArray = [
       source: 'See source',
     },
   },
-  // {
-  //   class: 'background-img2 background-img',
-  //   class2: 'card-img1',
-  //   subtitle: 'Multi-Post Stories',
-  //   description: 'A daily selection of privately personalized reads; no accounts or sign-ups required. has been the industry\'s standard dummy text ever since the 1500s, when an unknown printer took a standard dummy text.',
-  //   image: {
-  //     image1: './imgs/placeholder.png',
-  //   },
-  //   langlist: {
-  //     css: 'css',
-  //     html: 'html',
-  //     bootstrap: 'bootstrap',
-  //     ruby: 'Ruby',
-  //     lags: 'Ruby on rails',
-  //   },
-  //   btn: 'See Project',
-  //   style: {
-  //     style1: 'section-2',
-  //   },
-  //   imageleft: 'image-left',
-  // },
+  {
+    class: 'background-img2 background-img',
+    class2: 'card-img1',
+    subtitle: 'Multi-Post Stories',
+    description: 'A daily selection of privately personalized reads; no accounts or sign-ups required. has been the industry\'s standard dummy text ever since the 1500s, when an unknown printer took a standard dummy text.',
+    image: {
+      image1: './imgs/placeholder.png',
+    },
+    langlist: {
+      css: 'css',
+      html: 'html',
+      bootstrap: 'bootstrap',
+      ruby: 'Ruby',
+      lags: 'Ruby on rails',
+    },
+    btn: 'See Project',
+    style: {
+      style1: 'section-2',
+    },
+    imageleft: 'image-left',
+  },
   // {
   //   class: 'background-img3 background-img',
   //   class2: 'card-img',

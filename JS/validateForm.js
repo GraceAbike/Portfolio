978460ced8dc3fb14b4b413904375d2d8b525404
Yaser <khanyaser007@gmail.com>
2022-09-13 21:42:14 +0500

    Add validation message

--- JS/validateForm.js
+++ JS/validateForm.js
@@ -4,3 +4,9 @@ const validationMessage = document.querySelector('#error');
 validationMessage.style.color = '#ff0000';
 validationMessage.style.fontSize = '15px';
 validationMessage.style.padding = '4px';
+formContainer.addEventListener('submit', (e) => {
+    let isValid = false;
+    if (email.value === email.value.toLowerCase()) {
+      isValid = true;
+    }
+})

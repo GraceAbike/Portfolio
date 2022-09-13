6a68a35122feaed7aba6f608eaa918aa16dd7354
Grace Abike <mara234216@gmail.com>
2022-09-13 18:30:02 +0200

    Add Form Validate Section

---
+++ JS/validateForm.js
@@ -0,0 +1,6 @@
+const formContainer = document.querySelector('.form-container');
+const email = document.querySelector('#email');
+const validationMessage = document.querySelector('#error');
+validationMessage.style.color = '#ff0000';
+validationMessage.style.fontSize = '15px';
+validationMessage.style.padding = '4px';
